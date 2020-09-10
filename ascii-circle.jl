3bebc8948881a797ddd5fcfc8290dbcf452d9867
Spenser Black <spenserblack01@gmail.com>
2020-09-10 16:17:24 -0400

    Draw an oval

--- ascii-circle.jl
+++ ascii-circle.jl
@@ -4,4 +4,23 @@ r = try
     error("First arg is required and must be an integer")
 end
 
-println("radius:", r)
+h = 2r
+w = 2h # width is double height due to size of monospace characters
+origin = (h//2, w//2)
+x_origin, y_origin = origin
+
+is_in_circle(x::Int, y::Int) = begin
+  d = √((x_origin - x)^2 + (y_origin - y)^2)
+  d ≤ r
+end
+
+for x = 1:h
+  for y = 1:w
+    if is_in_circle(x, y)
+      print('o')
+    else
+      print(' ')
+    end
+  end
+  println()
+end
