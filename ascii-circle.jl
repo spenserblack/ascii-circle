cc62c0c8c6ca5df3fbc15562c2441a9d2b0650e6
Spenser Black <spenserblack01@gmail.com>
2020-09-11 09:36:27 -0400

    Add edges to circle

--- ascii-circle.jl
+++ ascii-circle.jl
@@ -11,18 +11,31 @@ w = h // wh_ratio
 origin = (h//2, w//2)
 x_origin, y_origin = origin
 
-is_in_circle(x::Number, y::Number) = begin
+get_char(x::Number, y::Number) = begin
   d = √(((x_origin - x)//wh_ratio)^2 + (y_origin - y)^2)
-  d ≤ r
+  d = Int(floor(d))
+  if d < r
+    'o'
+  elseif d == r
+    θorigin = atan((y_origin - y)//(x_origin - x))
+    θedge = π/2 + θorigin # 0 to π
+    if π/8 ≤ θedge < 3π/8
+      '\\'
+    elseif 3π/8 ≤ θedge ≤ 5π/8
+      '-'
+    elseif 5π/8 < θedge ≤ 7π/8
+      '/'
+    else
+      '|'
+    end
+  else
+    '.'
+  end
 end
 
 for x = 1:h
   for y = 1:w
-    if is_in_circle(x, y)
-      print('o')
-    else
-      print('.')
-    end
+    print(get_char(x,y))
   end
   println()
 end
